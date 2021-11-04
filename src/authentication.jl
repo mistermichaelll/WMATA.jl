9d013665c66c0eb2ffc5f7f02187550581f350b1
mistermichaelll <mkjohnson@mail.roanoke.edu>
2021-11-04 15:03:37 -0400

    Oops - left out some words in here!

--- src/authentication.jl
+++ src/authentication.jl
@@ -4,7 +4,7 @@ Function Name: WMATA_auth()
 Purpose: WMATA's API has an endpoint which allows us to verify whether a subscription key is valid, or if there is 
 an issue with their API. What I'd like to do here is make the other functions a bit less redundant by creating a function which 
 creates a global variable titled AuthToken. Instead of a user defining a "sub_key" and then calling that in every function in the package, 
-they should be able to 
+they should be able to run this function, verify that the token is valid, and then not have to call it for each function.
 =#
 function WMATA_auth(SubscriptionKey::String)
     url = "https://api.wmata.com/Misc/Validate"
