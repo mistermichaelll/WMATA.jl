7a4813887823567ca20b0e8803c4ae45a5a171f2
Michael Johnson <mistermichaellll@gmail.com>
2022-04-08 15:23:18 -0400

    adjust function names

--- src/utils.jl
+++ src/utils.jl
@@ -2,8 +2,9 @@
 since we're dealing with user input, it makes sense to build in some checks to the functions 
 so that mistakes are obvious and not mysterious.
 =#
+
 # verify that a linecode is one that makes sense
-function verify_line_inputs(line_input)
+function verify_line_input(line_input)
     line_colors = ["RD", "BL", "YL", "OR", "GR", "SV", "All"]
     if !(line_input in line_colors)
         error("ERROR: line must be one of: RD, BL, YL, OR, GR, SV, or All")
@@ -12,5 +13,19 @@ function verify_line_inputs(line_input)
     end
 end
 
+# 
+function verify_station_input(station_input)
+    # this function relies on the same endpoint that the station_list function does.
+    subscription_key = Dict("api_key" => WMATA_AuthToken)
+    r = request("GET", "https://api.wmata.com/Rail.svc/json/jStations", subscription_key)
+    r = parse(String(r.body))
+    
+    valid_station_codes = [station["Code"] for station in r["Stations"]]
 
+    if !(station_input in valid_station_codes)
+        error("ERROR: $station_input is not a valid station code.\nTry using station_list to find and verify your station code.")
+    else 
+        return(station_input)
+    end
+end
 
