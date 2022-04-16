6eba8017bf124253ee5ceaf35629cce631cbfd2d
Michael Johnson <mistermichaellll@gmail.com>
2022-04-16 13:47:28 -0400

    remove cruft

--- src/WMATA.jl
+++ src/WMATA.jl
@@ -5,4 +5,4 @@ include("rail.jl")
 import DataFrames.DataFrame, JSON.parse, HTTP.request
 export rail_predictions, station_list, station_timings, WMATA_auth, path_between, station_to_station
 
-end # module
+end
