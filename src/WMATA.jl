22337e6a01e943107a7f2fb7ef61dd93e6141a1a
Michael Johnson <mistermichaellll@gmail.com>
2022-04-18 15:31:30 -0400

    need this function

--- src/WMATA.jl
+++ src/WMATA.jl
@@ -2,7 +2,7 @@ module WMATA
 include("authentication.jl")
 include("rail.jl")
 
-import DataFrames.DataFrame, JSON.parse, HTTP.request, DataFrames.rename!, DataFrames.names
+import DataFrames.DataFrame, JSON.parse, HTTP.request, DataFrames.rename!, DataFrames.names, DataFrames.nrow
 export get_rail_predictions, get_station_list, get_station_timings, WMATA_auth, get_path_between, get_station_to_station, get_train_positions
 
 end
