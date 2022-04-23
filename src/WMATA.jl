1a49f382afe1cd801e24a63febd154282a16fa28
Michael Johnson <mistermichaellll@gmail.com>
2022-04-23 10:50:00 -0400

    more sensible grouping in the module

--- src/WMATA.jl
+++ src/WMATA.jl
@@ -3,7 +3,8 @@ include("authentication.jl")
 include("rail.jl")
 
 import DataFrames.DataFrame, JSON.parse, HTTP.request, DataFrames.rename!, DataFrames.names, DataFrames.nrow
-export get_rail_predictions, get_station_list, get_station_timings, WMATA_auth, get_path_between, get_station_to_station, get_train_positions
-export get_rail_incidents
+export WMATA_auth
+export get_rail_predictions, get_station_list, get_station_timings, get_path_between, get_station_to_station
+export get_rail_incidents, get_train_positions
 
 end
