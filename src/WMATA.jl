9a737976449811468529493ad69080eaa2daaafc
Michael Johnson <mistermichaellll@gmail.com>
2022-04-25 18:13:32 -0400

    allow users to use wmata_request

--- src/WMATA.jl
+++ src/WMATA.jl
@@ -6,5 +6,6 @@ import DataFrames.DataFrame, JSON.parse, HTTP.request, DataFrames.rename!, DataF
 export WMATA_auth
 export get_rail_predictions, get_station_list, get_station_timings, get_path_between, get_station_to_station
 export get_rail_incidents, get_train_positions
+export wmata_request
 
 end
