301cfaffd64226b2d72f6a984d1d0e7e027590b0
Michael Johnson <mistermichaellll@gmail.com>
2023-05-20 18:35:44 -0400

    add json 3

--- src/WMATA.jl
+++ src/WMATA.jl
@@ -2,7 +2,8 @@ module WMATA
 include("authentication.jl")
 include("rail.jl")
 
-import DataFrames.DataFrame, JSON.parse, HTTP.request, DataFrames.rename!, DataFrames.names, DataFrames.nrow
+import DataFrames.DataFrame, HTTP.request, DataFrames.rename!, DataFrames.names, DataFrames.nrow
+using JSON3
 export WMATA_auth
 export get_rail_predictions, get_station_list, get_station_timings, get_path_between, get_station_to_station
 export get_rail_incidents, get_train_positions
