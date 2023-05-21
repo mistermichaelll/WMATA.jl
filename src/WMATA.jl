694bb649bbf6df2d7c8b45638b2cf206fbeb02e5
Michael Johnson <mistermichaellll@gmail.com>
2023-05-21 16:13:04 -0400

    update imports

--- src/WMATA.jl
+++ src/WMATA.jl
@@ -2,7 +2,8 @@ module WMATA
 include("authentication.jl")
 include("rail.jl")
 
-import DataFrames.DataFrame, HTTP.request, DataFrames.rename!, DataFrames.names, DataFrames.nrow
+import HTTP.request
+using DataFrames
 using JSON3
 export WMATA_auth
 export get_rail_predictions, get_station_list, get_station_timings, get_path_between, get_station_to_station
