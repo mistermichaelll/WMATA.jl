f13d3b1c3a8709abec98b4bf5e9bf6db755aa104
Michael Johnson <mistermichaellll@gmail.com>
2023-05-21 16:32:39 -0400

    remove from exports

--- src/WMATA.jl
+++ src/WMATA.jl
@@ -6,7 +6,7 @@ import HTTP.request
 using DataFrames
 using JSON3
 export WMATA_auth
-export get_rail_predictions, get_station_list, get_station_timings, get_path_between, get_station_to_station
+export get_rail_predictions, get_station_list, get_path_between, get_station_to_station
 export get_rail_incidents, get_train_positions
 export wmata_request
 
